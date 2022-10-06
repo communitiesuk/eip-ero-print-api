b1397a5ad66c8d2b68e54d3495717866bcdc5359
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2022-10-06 12:40:41 +0100

    EIP1-1926 - Basic spring boot app (#2)
    * EIP1-1926 - Basic spring boot app
    
    * EIP1-1926 - Added initial openAPI spec with health check endpoint
    
    Co-authored-by: Nathan Russell <nathan@.russell@valtech.com>

---
+++ README.md
@@ -0,0 +1,34 @@
+# eip-ero-print-api
+Spring Boot microservice that :
+- Provides an API for sending applications to print.
+
+## Developer Setup
+### Kotlin API Developers
+
+Configure your IDE with the code formatter (ktlint):
+```
+$ ./gradlew ktlintApplyToIdea
+```
+This only needs doing once to setup your IDE with the code styles.
+
+#### Running Tests
+```
+$ ./gradlew check
+```
+This will run the tests and ktlint. (Be warned, ktlint will hurt your feelings!)
+
+#### Building docker images
+```
+$ ./gradlew check bootBuildImage
+```
+This will build a docker image for the Spring Boot application.
+
+## Running the application
+Either `./gradlew bootRun` or run the class `PrintApiApplication`
+
+### Authentication and authorisation
+Requests are authenticated by the presence of a signed cognito JWT as a bearer token in the HTTP request `authorization` header.  
+EG: `Authorization: Bearer xxxxxyyyyyyzzzzz.....`  
+Requests are authorised by their membership of groups and roles carried on the JWT token.  
+The UI application is expected to handle the authentication with cognito and pass the JWT token in the `authorization` header.
+
---
+++ build.gradle.kts
@@ -0,0 +1,166 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jlleitschuh.gradle.ktlint.tasks.KtLintCheckTask
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+import org.owasp.dependencycheck.reporting.ReportGenerator.Format.HTML
+
+plugins {
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    kotlin("jvm") version "1.7.0"
+    kotlin("kapt") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.0"
+    kotlin("plugin.jpa") version "1.7.0"
+    kotlin("plugin.allopen") version "1.7.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("org.jlleitschuh.gradle.ktlint-idea") version "10.3.0"
+    id("org.openapi.generator") version "6.0.1"
+    id("org.owasp.dependencycheck") version "7.1.2"
+}
+
+group = "uk.gov.dluhc"
+version = "latest"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+ext["snakeyaml.version"] = "1.33"
+extra["springCloudVersion"] = "2.4.2"
+extra["awsSdkVersion"] = "2.17.284"
+
+allOpen {
+    annotations("javax.persistence.Entity", "javax.persistence.MappedSuperclass", "javax.persistence.Embedabble")
+}
+
+repositories {
+    mavenCentral()
+}
+
+apply(plugin = "org.jlleitschuh.gradle.ktlint")
+apply(plugin = "org.openapi.generator")
+apply(plugin = "org.springframework.boot")
+apply(plugin = "io.spring.dependency-management")
+apply(plugin = "org.jetbrains.kotlin.jvm")
+apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+apply(plugin = "org.jetbrains.kotlin.plugin.allopen")
+
+dependencies {
+    // framework
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.1.23")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.mapstruct:mapstruct:1.5.2.Final")
+    kapt("org.mapstruct:mapstruct-processor:1.5.2.Final")
+
+    // api
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // spring security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+
+    // AWS v2 dependencies
+    implementation("software.amazon.awssdk:dynamodb")
+    implementation("software.amazon.awssdk:dynamodb-enhanced")
+
+    // Test implementations
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    testImplementation("org.testcontainers:junit-jupiter:1.17.3")
+    testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+
+    testImplementation("org.testcontainers:testcontainers:1.17.4")
+
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.2")
+    testImplementation("net.datafaker:datafaker:1.6.0")
+
+    // Libraries to support creating JWTs in tests
+    testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("io.awspring.cloud:spring-cloud-aws-dependencies:${property("springCloudVersion")}")
+        mavenBom("software.amazon.awssdk:bom:${property("awsSdkVersion")}")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    dependsOn(tasks.withType<GenerateTask>())
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    dependsOn(tasks.withType<GenerateTask>())
+    useJUnitPlatform()
+}
+
+tasks.withType<GenerateTask> {
+    enabled = false
+    validateSpec.set(true)
+    outputDir.set("$projectDir/build/generated")
+    generatorName.set("kotlin-spring")
+    generateModelTests.set(false)
+    generateModelDocumentation.set(false)
+    globalProperties.set(
+        mapOf(
+            "apis" to "false",
+            "invokers" to "false",
+            "models" to "",
+        )
+    )
+    configOptions.set(
+        mapOf(
+            "dateLibrary" to "java8",
+            "serializationLibrary" to "jackson",
+            "enumPropertyNaming" to "UPPERCASE",
+            "useBeanValidation" to "true",
+        )
+    )
+}
+
+// Add the generated code to the source sets
+sourceSets["main"].java {
+    this.srcDir("$projectDir/build/generated")
+}
+
+// Linting is dependent on GenerateTask
+tasks.withType<KtLintCheckTask> {
+    dependsOn(tasks.withType<GenerateTask>())
+}
+
+// Exclude generated code from linting
+ktlint {
+    filter {
+        exclude { projectDir.toURI().relativize(it.file.toURI()).path.contains("/generated/") }
+    }
+}
+
+kapt {
+    arguments {
+        arg("mapstruct.defaultComponentModel", "spring")
+        arg("mapstruct.unmappedTargetPolicy", "IGNORE")
+    }
+}
+
+/* Configuration for the OWASP dependency check */
+dependencyCheck {
+    autoUpdate = true
+    failOnError = true
+    failBuildOnCVSS = 0.toFloat()
+    analyzers.assemblyEnabled = false
+    analyzers.centralEnabled = true
+    format = HTML
+    suppressionFiles = listOf("owasp.suppressions.xml")
+}
