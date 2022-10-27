30df30384bd86886f9ecc7a000f2c4c6d8c88a59
Abdullah Sanver <abdullah.sanver@valtech.com>
2022-10-27 10:41:24 +0100

    Implement PrintService

---
+++ src/main/kotlin/uk/gov/dluhc/printapi/service/PrintService.kt
@@ -0,0 +1,19 @@
+package uk.gov.dluhc.printapi.service
+
+import uk.gov.dluhc.printapi.database.repository.PrintDetailsRepository
+import uk.gov.dluhc.printapi.mapper.PrintDetailsMapper
+import uk.gov.dluhc.printapi.messaging.models.SendApplicationToPrintMessage
+import uk.gov.dluhc.printapi.client.ElectoralRegistrationOfficeManagementApiClient as EroClient
+
+class PrintService(
+    private val eroClient: EroClient,
+    private val printDetailsMapper: PrintDetailsMapper,
+    private val printDetailsRepository: PrintDetailsRepository
+) {
+    fun savePrintMessage(message: SendApplicationToPrintMessage) {
+        val ero = eroClient.getElectoralRegistrationOffice(message.gssCode!!)
+        val localAuthority = ero.localAuthorities.first { it.gssCode == message.gssCode }
+        val printDetails = printDetailsMapper.toPrintDetails(message, ero, localAuthority.name)
+        printDetailsRepository.save(printDetails)
+    }
+}
