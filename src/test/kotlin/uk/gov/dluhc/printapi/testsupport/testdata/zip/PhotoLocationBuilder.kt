ad1faa0b1d4b5b2150dfd15b42199fe77f2f7e46
Neil-Massey <54864568+Neil-Massey@users.noreply.github.com>
2022-10-28 13:13:54 +0100

    EIP1-1878 Add creation of file details (#24)

--- src/test/kotlin/uk/gov/dluhc/printapi/testsupport/testdata/zip/PhotoLocationBuilder.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/testsupport/testdata/zip/PhotoLocationBuilder.kt
@@ -1,5 +1,17 @@
 package uk.gov.dluhc.printapi.testsupport.testdata.zip
 
+import uk.gov.dluhc.printapi.service.PhotoLocation
+
+fun photoLocationBuilder(
+    zipPath: String = aPhotoZipPath(),
+    sourceBucket: String = aPhotoBucket(),
+    sourcePath: String = aPhotoBucketPath()
+) = PhotoLocation(zipPath, sourceBucket, sourcePath)
+
+fun aPhotoLocation(): PhotoLocation = photoLocationBuilder()
+
+fun aPhotoLocationList(): List<PhotoLocation> = listOf(aPhotoLocation())
+
 fun aPhotoArn() = "arn:aws:s3:::dev-vca-api-vca-target-bucket/E09000007/0013a30ac9bae2ebb9b1239b/0d77b2ad-64e7-4aa9-b4de-d58380392962/8a53a30ac9bae2ebb9b1239b-initial-photo-1.png"
 
 fun aPhotoBucket() = "dev-vca-api-vca-target-bucket"
