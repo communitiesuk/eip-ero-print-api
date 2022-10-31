c25a85b9efaf19f89d503235abfd24938e356136
Fayi FB <fayi@fayimora.com>
2022-10-31 09:41:03 +0000

    EIP1-2214: Implement job scheduler (#18)
    * EIP1-2214: Implement job scheduler
    
    * Schedule locks is temporary disabled
    
    * EIP1-2214: PR comments
    
    * EIP1-2214: PR comments
    
    * EIP1-2214: Disable scheduler for tests

--- README.md
+++ README.md
@@ -33,8 +33,11 @@ The following environment variables must be set in order to run the application:
 * `API_ERO_MANAGEMENT_URL` - the base URL of the ERO Management REST API service.
 * `DYNAMODB_ENDPOINT` - the localstack endpoint
 * `DYNAMODB_PRINT_DETAILS_TABLE_NAME` - table name to persist print details
+* `DYNAMODB_SCHEDULER_LOCKS_TABLE_NAME` - table name to persist scheduler locks
+* `JOB_PRINT_REQUESTS_CRON` - cron expression for print request job
 * `THREAD_POOL_ZIP_CORE_SIZE` - number of core threads for the Zip producer thread pool 
 * `THREAD_POOL_ZIP_MAX_SIZE` - maximum number of threads for the Zip producer thread pool
+
 
 ### Authentication and authorisation
 Requests are authenticated by the presence of a signed cognito JWT as a bearer token in the HTTP request `authorization` header.  
---
+++ src/main/kotlin/uk/gov/dluhc/printapi/config/SchedulerLockConfiguration.kt
@@ -0,0 +1,24 @@
+package uk.gov.dluhc.printapi.config
+
+import net.javacrumbs.shedlock.core.LockProvider
+import net.javacrumbs.shedlock.provider.dynamodb2.DynamoDBLockProvider
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+
+@Configuration
+@EnableScheduling
+@ConditionalOnProperty("jobs.enabled", havingValue = "true")
+// @EnableSchedulerLock(defaultLockAtMostFor = "PT5M")
+class SchedulerLockConfiguration(
+    private val dynamoDbClient: DynamoDbClient,
+    private val dynamoDbConfiguration: DynamoDbConfiguration
+) {
+
+    @Bean
+    fun schedulerLockProvider(): LockProvider {
+        return DynamoDBLockProvider(dynamoDbClient, dynamoDbConfiguration.schedulerLockTableName)
+    }
+}
