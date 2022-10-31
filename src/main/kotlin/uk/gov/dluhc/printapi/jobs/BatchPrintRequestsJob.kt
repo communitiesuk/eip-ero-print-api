bdae068bf49bb9010c53a34ff5be6d08c73b46d2
Fayi Femi-Balogun <fayi@fayimora.com>
2022-10-31 15:28:55 +0000

    EIP1-2214: Cleanup unused scheduler lock options

--- src/main/kotlin/uk/gov/dluhc/printapi/jobs/BatchPrintRequestsJob.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/jobs/BatchPrintRequestsJob.kt
@@ -10,7 +10,7 @@ private val logger = KotlinLogging.logger {}
 class BatchPrintRequestsJob {
 
     @Scheduled(cron = "\${jobs.print-requests.cron}")
-//    @SchedulerLock(name = "\${jobs.print-requests.name}", lockAtMostFor = "\${jobs.print-requests.lockAtMostFor}", lockAtLeastFor = "\${jobs.print-requests.lockAtLeastFor}")
+//    @SchedulerLock(name = "\${jobs.print-requests.name}")
     fun run() {
         logger.info { "Hello, I'm your batching agent" }
     }
