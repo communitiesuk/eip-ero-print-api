2a2f07f0576f6414e4bbcecea4a757e4531c9ba1
Fayi Femi-Balogun <fayi@fayimora.com>
2022-11-10 14:26:10 +0000

    EIP1-2417: Parse response file

--- src/test/kotlin/uk/gov/dluhc/printapi/service/PrintResponseFileServiceTest.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/service/PrintResponseFileServiceTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.dluhc.printapi.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
@@ -8,11 +9,18 @@ import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.any
 import org.mockito.kotlin.given
 import org.mockito.kotlin.verify
+import uk.gov.dluhc.printapi.printprovider.models.PrintResponses
 
 @ExtendWith(MockitoExtension::class)
 internal class PrintResponseFileServiceTest {
     @Mock
     private lateinit var sftpService: SftpService
+
+    @Mock
+    private lateinit var objectMapper: ObjectMapper
+
+    @Mock
+    private lateinit var printResponseProcessingService: PrintResponseProcessingService
 
     @InjectMocks
     private lateinit var printResponseFileService: PrintResponseFileService
@@ -25,6 +33,8 @@ internal class PrintResponseFileServiceTest {
         val fileContent = "{\"BatchResponses\": [], \"PrintResponses\": []}"
 
         given(sftpService.fetchFileFromOutBoundDirectory(any(), any())).willReturn(fileContent)
+        given(objectMapper.readValue(fileContent, PrintResponses::class.java))
+            .willReturn(PrintResponses().withBatchResponses(emptyList()).withPrintResponses(emptyList()))
 
         // When
         printResponseFileService.processPrintResponseFile(directory, fileName)
