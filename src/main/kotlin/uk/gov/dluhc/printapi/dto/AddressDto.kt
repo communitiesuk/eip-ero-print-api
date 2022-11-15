b1a165b3d90f70b103cd1b573357ae51825ef458
Nathan Russell <nathan.russell@valtech.com>
2022-11-15 14:14:30 +0000

    EIP1-2533 - Addressed PR comments

--- src/main/kotlin/uk/gov/dluhc/printapi/dto/AddressDto.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/dto/AddressDto.kt
@@ -3,7 +3,7 @@ package uk.gov.dluhc.printapi.dto
 data class AddressDto(
     var street: String,
     var postcode: String,
-    var `property`: String? = null,
+    var property: String? = null,
     var locality: String? = null,
     var town: String? = null,
     var area: String? = null,
