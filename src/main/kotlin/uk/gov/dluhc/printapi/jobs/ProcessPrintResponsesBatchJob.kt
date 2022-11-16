708b7d4d77c68f5b87282e0d2db7709113a82b29
Nathan Russell <nathan.russell@valtech.com>
2022-11-16 13:10:57 +0000

    EIP1-1878 - Logging in scheduled services; adjusted cron times in dev

--- src/main/kotlin/uk/gov/dluhc/printapi/jobs/ProcessPrintResponsesBatchJob.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/jobs/ProcessPrintResponsesBatchJob.kt
@@ -1,12 +1,9 @@
 package uk.gov.dluhc.printapi.jobs
 
-import mu.KotlinLogging
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import uk.gov.dluhc.printapi.service.PrintResponseFileReadinessService
-
-private val logger = KotlinLogging.logger {}
 
 @Component
 class ProcessPrintResponsesBatchJob(
@@ -16,8 +13,6 @@ class ProcessPrintResponsesBatchJob(
     @Scheduled(cron = "\${jobs.process-print-responses.cron}")
     @SchedulerLock(name = "\${jobs.process-print-responses.name}")
     fun pollAndProcessPrintResponses() {
-        logger.info { "Polling for print responses from outbound directory" }
         printResponseFileReadinessService.markAndSubmitPrintResponseFileForProcessing()
-        logger.info { "Completed print response polling job from outbound directory" }
     }
 }
