63201cc47c19f4cccd189e48f9c51b6c7dfc664a
Abdullah Sanver <39367710+adacayi@users.noreply.github.com>
2022-11-18 15:11:30 +0000

    EIP1-1990: EIP1-2594: Implement basic get certificate summary by application id endpoint (#80)

---
+++ src/main/kotlin/uk/gov/dluhc/printapi/rest/CertificateSummaryController.kt
@@ -0,0 +1,32 @@
+package uk.gov.dluhc.printapi.rest
+
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.dluhc.printapi.models.CertificateSummaryResponse
+
+@RestController
+@CrossOrigin
+class CertificateSummaryController {
+    companion object {
+        const val ERO_VC_ADMIN_GROUP_PREFIX = "ero-vc-admin-"
+    }
+
+    @GetMapping("/eros/{eroId}/certificates/applications/{applicationId}")
+    @PreAuthorize(
+        """
+        hasAnyAuthority(
+            T(uk.gov.dluhc.printapi.rest.CertificateSummaryController).ERO_VC_ADMIN_GROUP_PREFIX.concat(#eroId)
+        )
+        """
+    )
+    fun getCertificateSummaryByApplicationId(
+        @PathVariable eroId: String,
+        @PathVariable applicationId: String,
+    ): CertificateSummaryResponse {
+        // TODO: Will be implemented as part of EIP1-2597
+        return CertificateSummaryResponse("", listOf())
+    }
+}
