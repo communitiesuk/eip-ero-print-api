47c83ba590567e37858596a64367e23e285c6d2a
Nathan Russell <nathan.russell@valtech.com>
2022-11-22 08:27:08 +0000

    Got AOP Advice for sending SQS messages working

--- src/main/kotlin/uk/gov/dluhc/printapi/config/CorrelationIdMdcConfiguration.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/config/CorrelationIdMdcConfiguration.kt
@@ -1,10 +1,13 @@
 package uk.gov.dluhc.printapi.config
 
 import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.slf4j.MDC
 import org.springframework.messaging.Message
+import org.springframework.messaging.support.GenericMessage
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 import java.lang.Exception
@@ -51,7 +54,7 @@ class CorrelationIdMdcInterceptor : HandlerInterceptor {
 class CorrelationIdMdcMessageListenerAspect {
 
     /**
-     * Pointcut for inbound [Message]s (ie. SQS Message's being directed to a listener class) that sets the correlation ID
+     * Before Advice for inbound [Message]s (ie. SQS Message's being directed to a listener class) that sets the correlation ID
      * MDC variable to the value found in the Message header `x-correlation-id` if set, or a new value.
      * This allows for passing and logging a consistent correlation ID between disparate systems or processes.
      */
@@ -62,27 +65,38 @@ class CorrelationIdMdcMessageListenerAspect {
     }
 
     /**
-     * Pointcut for outbound [Message]s (ie. SQS Message's being sent) that sets the correlation ID
-     * header on the [Message] to either the existing MDC variable or a new value if not set in MDC.
+     * Around Advice for outbound [Message]s (ie. SQS Message's being sent) that sets the correlation ID
+     * header on a new [Message] to either the existing MDC variable or a new value if not set in MDC.
      * This allows for passing and logging a consistent correlation ID between disparate systems or processes.
+     *
+     * The reason this Advice is an Around is because [Message] and it's headers are immutable, so we cannot add
+     * the correlation ID header on the passed [Message]. Therefore we need to create new message with the same
+     * payload and a modified collection of headers.
      */
-    @Before("execution(* org.springframework.messaging.support.AbstractMessageChannel.send(..))")
-    // @Before("execution(* io.awspring.cloud.messaging.core.QueueMessageChannel.send(..))")
-    fun beforeSendMessage(joinPoint: JoinPoint) {
-        val message = joinPoint.args[0] as Message<*>?
-        message?.headers?.put(CORRELATION_ID_HEADER, getCurrentCorrelationId())
+    @Around("execution(* io.awspring.cloud.messaging.core.support.AbstractMessageChannelMessagingSendingTemplate.send(..))")
+    fun aroundSendMessage(proceedingJoinPoint: ProceedingJoinPoint): Any? {
+        val queue = proceedingJoinPoint.args[0]
+        val originalMessage = proceedingJoinPoint.args[1] as Message<*>
+        val newMessage = GenericMessage(
+            originalMessage.payload,
+            originalMessage.headers.toMutableMap().plus(CORRELATION_ID_HEADER to getCurrentCorrelationId())
+        )
+        return proceedingJoinPoint.proceed(arrayOf(queue, newMessage))
     }
 }
 
 /**
  * AOP Aspect for Scheduled tasks (ie. cron tasks) that sets the correlation ID MDC variable.
- * Due to the invocation semantics of a Scheduled task it does not make sense to pass a correlation ID from another
- * system or process into it.
  */
 @Aspect
 @Component
 class CorrelationIdMdcScheduledAspect {
 
+    /**
+     * Before Advice for Scheduled tasks (ie. cron tasks) that sets the correlation ID MDC variable to a new value.
+     * Due to the invocation semantics of a Scheduled task it does not make sense to pass a correlation ID from another
+     * system or process into it.
+     */
     @Before("@annotation(org.springframework.scheduling.annotation.Scheduled)")
     fun before(joinPoint: JoinPoint) {
         MDC.put(CORRELATION_ID, generateCorrelationId())
@@ -92,5 +106,5 @@ class CorrelationIdMdcScheduledAspect {
 private fun generateCorrelationId(): String =
     UUID.randomUUID().toString().replace("-", "")
 
-private fun getCurrentCorrelationId() : String =
+private fun getCurrentCorrelationId(): String =
     MDC.get(CORRELATION_ID_HEADER) ?: generateCorrelationId()
