077126d739541e84017280596fa46a69cddea3e9
Neil Massey <neil.massey@valtech.com>
2022-11-28 14:52:45 +0000

    EIP1-2663 Integrate latest ERO Management API

--- src/test/kotlin/uk/gov/dluhc/printapi/testsupport/testdata/model/LocalAuthorityResponseBuilder.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/testsupport/testdata/model/LocalAuthorityResponseBuilder.kt
@@ -1,10 +1,53 @@
 package uk.gov.dluhc.printapi.testsupport.testdata.model
 
+import uk.gov.dluhc.eromanagementapi.models.Address
+import uk.gov.dluhc.eromanagementapi.models.ContactDetails
 import uk.gov.dluhc.eromanagementapi.models.LocalAuthorityResponse
+import uk.gov.dluhc.printapi.testsupport.testdata.DataFaker.Companion.faker
+import uk.gov.dluhc.printapi.testsupport.testdata.aValidEmailAddress
 import uk.gov.dluhc.printapi.testsupport.testdata.aValidLocalAuthorityName
+import uk.gov.dluhc.printapi.testsupport.testdata.aValidPhoneNumber
+import uk.gov.dluhc.printapi.testsupport.testdata.aValidWebsite
 import uk.gov.dluhc.printapi.testsupport.testdata.getRandomGssCode
 
 fun buildLocalAuthorityResponse(
     gssCode: String = getRandomGssCode(),
     name: String = aValidLocalAuthorityName()
-) = LocalAuthorityResponse(gssCode = gssCode, name = name)
+) = LocalAuthorityResponse(
+    gssCode = gssCode,
+    name = name,
+    contactDetailsEnglish = buildContactDetails(),
+    contactDetailsWelsh = null,
+)
+
+fun buildContactDetails(
+    websiteAddress: String = aValidWebsite(),
+    phoneNumber: String = aValidPhoneNumber(),
+    emailAddress: String = aValidEmailAddress(),
+    address: Address = buildEroManagementAddress(),
+): ContactDetails =
+    ContactDetails(
+        website = websiteAddress,
+        phone = phoneNumber,
+        email = emailAddress,
+        address = address
+    )
+
+fun buildEroManagementAddress(
+    street: String = faker.address().streetName(),
+    postcode: String = faker.address().postcode(),
+    property: String = faker.address().buildingNumber(),
+    town: String = faker.address().city(),
+    area: String = faker.address().state(),
+    locality: String? = null,
+    uprn: String? = null,
+): Address =
+    Address(
+        street = street,
+        postcode = postcode,
+        property = property,
+        area = area,
+        town = town,
+        locality = locality,
+        uprn = uprn
+    )
