ee214b557a0eb32dc29b21c7f215afed6fe519e9
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-02-06 10:38:46 +0000

    EIP1-3439 / EIP1-3722 - Request validation with 400 scenarios (#128)

--- src/main/kotlin/uk/gov/dluhc/printapi/rest/TemporaryCertificateController.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/rest/TemporaryCertificateController.kt
@@ -1,10 +1,14 @@
 package uk.gov.dluhc.printapi.rest
 
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.dluhc.printapi.models.GenerateTemporaryCertificateRequest
+import javax.validation.Valid
 
 @RestController
 @CrossOrigin
@@ -12,7 +16,12 @@ class TemporaryCertificateController {
 
     @PostMapping("/eros/{eroId}/temporary-certificate")
     @PreAuthorize(HAS_ERO_VC_ADMIN_AUTHORITY)
-    fun generateTemporaryCertificate(@PathVariable eroId: String) {
+    fun generateTemporaryCertificate(
+        @PathVariable eroId: String,
+        @RequestBody @Valid generateTemporaryCertificateRequest: GenerateTemporaryCertificateRequest,
+        authentication: Authentication
+    ) {
+        val userId = authentication.name
         TODO("not yet implemented")
     }
 }
--- src/test/kotlin/uk/gov/dluhc/printapi/rest/GenerateTemporaryCertificateIntegrationTest.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/rest/GenerateTemporaryCertificateIntegrationTest.kt
@@ -3,10 +3,14 @@ package uk.gov.dluhc.printapi.rest
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import uk.gov.dluhc.printapi.config.IntegrationTest
+import uk.gov.dluhc.printapi.models.ErrorResponse
+import uk.gov.dluhc.printapi.testsupport.assertj.assertions.ErrorResponseAssert.Companion.assertThat
 import uk.gov.dluhc.printapi.testsupport.bearerToken
 import uk.gov.dluhc.printapi.testsupport.testdata.UNAUTHORIZED_BEARER_TOKEN
 import uk.gov.dluhc.printapi.testsupport.testdata.anotherValidEroId
 import uk.gov.dluhc.printapi.testsupport.testdata.getBearerToken
+import uk.gov.dluhc.printapi.testsupport.testdata.model.buildGenerateTemporaryCertificateRequest
+import uk.gov.dluhc.printapi.testsupport.withBody
 
 internal class GenerateTemporaryCertificateIntegrationTest : IntegrationTest() {
 
@@ -19,6 +23,7 @@ internal class GenerateTemporaryCertificateIntegrationTest : IntegrationTest() {
     fun `should return forbidden given no bearer token`() {
         webTestClient.post()
             .uri(URI_TEMPLATE, ERO_ID)
+            .withBody(buildGenerateTemporaryCertificateRequest())
             .exchange()
             .expectStatus()
             .isForbidden
@@ -31,6 +36,7 @@ internal class GenerateTemporaryCertificateIntegrationTest : IntegrationTest() {
             .uri(URI_TEMPLATE, ERO_ID)
             .bearerToken(UNAUTHORIZED_BEARER_TOKEN)
             .contentType(MediaType.APPLICATION_JSON)
+            .withBody(buildGenerateTemporaryCertificateRequest())
             .exchange()
             .expectStatus()
             .isUnauthorized
@@ -44,6 +50,7 @@ internal class GenerateTemporaryCertificateIntegrationTest : IntegrationTest() {
             .uri(URI_TEMPLATE, ERO_ID)
             .bearerToken(getBearerToken(eroId = ERO_ID, groups = listOf("ero-$ERO_ID", "ero-admin-$ERO_ID")))
             .contentType(MediaType.APPLICATION_JSON)
+            .withBody(buildGenerateTemporaryCertificateRequest())
             .exchange()
             .expectStatus()
             .isForbidden
@@ -63,8 +70,38 @@ internal class GenerateTemporaryCertificateIntegrationTest : IntegrationTest() {
                 )
             )
             .contentType(MediaType.APPLICATION_JSON)
+            .withBody(buildGenerateTemporaryCertificateRequest())
             .exchange()
             .expectStatus()
             .isForbidden
+    }
+
+    @Test
+    fun `should return bad request given invalid request body`() {
+        // Given
+        wireMockService.stubCognitoJwtIssuerResponse()
+        val invalidGssCode = "invalid GSS Code"
+        val requestBody = buildGenerateTemporaryCertificateRequest(
+            gssCode = invalidGssCode
+        )
+
+        // When
+        val response = webTestClient.post()
+            .uri(URI_TEMPLATE, ERO_ID)
+            .bearerToken(getBearerToken(eroId = ERO_ID, groups = listOf("ero-$ERO_ID", "ero-vc-admin-$ERO_ID")))
+            .contentType(MediaType.APPLICATION_JSON)
+            .withBody(requestBody)
+            .exchange()
+            .expectStatus()
+            .isBadRequest
+            .returnResult(ErrorResponse::class.java)
+
+        // Then
+        val actual = response.responseBody.blockFirst()
+        assertThat(actual)
+            .hasStatus(400)
+            .hasError("Bad Request")
+            .hasMessageContaining("Validation failed for object='generateTemporaryCertificateRequest'. Error count: 1")
+            .hasValidationError("Error on field 'gssCode': rejected value [$invalidGssCode], size must be between 9 and 9")
     }
 }
