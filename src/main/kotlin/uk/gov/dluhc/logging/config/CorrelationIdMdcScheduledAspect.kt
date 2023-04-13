3f2230bd50368ae0846ef867a9206fcb633db2b6
Neil-Massey <54864568+Neil-Massey@users.noreply.github.com>
2023-04-13 10:05:33 +0100

    EIP1-5345 Refactor the logging to be in separate package for reuse. (#220)

---
+++ src/main/kotlin/uk/gov/dluhc/logging/config/CorrelationIdMdcScheduledAspect.kt
@@ -0,0 +1,28 @@
+package uk.gov.dluhc.logging.config
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.slf4j.MDC
+
+/**
+ * AOP Aspect for Scheduled tasks (ie. cron tasks) that sets the correlation ID MDC variable.
+ *
+ * Copy of https://github.com/cabinetoffice/eip-ero-portal/blob/main/logging-lib/src/main/kotlin/uk/gov/dluhc/logging/config/CorrelationIdMdcScheduledAspect.kt
+ */
+@Aspect
+class CorrelationIdMdcScheduledAspect {
+
+    /**
+     * Around Advice for Scheduled tasks (ie. cron tasks) that sets the correlation ID MDC variable to a new value.
+     * Due to the invocation semantics of a Scheduled task it does not make sense to pass a correlation ID from another
+     * system or process into it.
+     */
+    @Around("@annotation(org.springframework.scheduling.annotation.Scheduled)")
+    fun aroundScheduledTask(proceedingJoinPoint: ProceedingJoinPoint): Any? {
+        MDC.put(CORRELATION_ID, generateCorrelationId())
+        return proceedingJoinPoint.proceed(proceedingJoinPoint.args).also {
+            MDC.remove(CORRELATION_ID)
+        }
+    }
+}
