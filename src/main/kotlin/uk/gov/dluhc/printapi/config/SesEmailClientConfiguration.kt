119d010ebc97cd78a2d7162821f17f25e8bf8b4d
NathanRussellValtech <78348259+NathanRussellValtech@users.noreply.github.com>
2023-04-27 15:03:25 +0100

    EIP1-4998 - Fix config; apply SES client endpointOverride (#249)

--- src/main/kotlin/uk/gov/dluhc/printapi/config/SesEmailClientConfiguration.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/config/SesEmailClientConfiguration.kt
@@ -1,10 +1,12 @@
 package uk.gov.dluhc.printapi.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.regions.providers.DefaultAwsRegionProviderChain
 import software.amazon.awssdk.services.ses.SesClient
 import uk.gov.dluhc.emailnotifications.SesEmailClient
+import java.net.URI
 
 /**
  * Configuration class exposing a configured email client bean to send emails via AWS's SES service.
@@ -12,9 +14,10 @@ import uk.gov.dluhc.emailnotifications.SesEmailClient
 @Configuration
 class SesEmailClientConfiguration() {
     @Bean
-    fun sesClient(): SesClient =
+    fun sesClient(@Value("\${cloud.aws.mail.endpoint}") sesVpcEndpoint: URI): SesClient =
         SesClient.builder()
             .region(DefaultAwsRegionProviderChain().region)
+            .endpointOverride(sesVpcEndpoint)
             .build()
 
     @Bean
