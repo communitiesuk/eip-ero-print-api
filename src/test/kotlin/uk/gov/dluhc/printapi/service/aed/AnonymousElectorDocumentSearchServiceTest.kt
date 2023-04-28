c98ba0803508bc0083605551f957ca8f2d442413
Vishal Gupta <vishal.gupta@valtech.com>
2023-04-28 16:28:59 +0100

    EIP1-5591: EIP1-5755: Service impl to search AEDs by surname

--- src/test/kotlin/uk/gov/dluhc/printapi/service/aed/AnonymousElectorDocumentSearchServiceTest.kt
+++ src/test/kotlin/uk/gov/dluhc/printapi/service/aed/AnonymousElectorDocumentSearchServiceTest.kt
@@ -10,10 +10,12 @@ import org.mockito.kotlin.any
 import org.mockito.kotlin.given
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
+import org.mockito.kotlin.verifyNoMoreInteractions
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import uk.gov.dluhc.printapi.database.entity.SourceType.ANONYMOUS_ELECTOR_DOCUMENT
 import uk.gov.dluhc.printapi.database.repository.AnonymousElectorDocumentSummaryRepository
+import uk.gov.dluhc.printapi.dto.aed.AedSearchBy
 import uk.gov.dluhc.printapi.dto.aed.AnonymousSearchSummaryResults
 import uk.gov.dluhc.printapi.mapper.aed.AnonymousSearchSummaryMapper
 import uk.gov.dluhc.printapi.service.EroService
@@ -40,11 +42,11 @@ internal class AnonymousElectorDocumentSearchServiceTest {
     private lateinit var anonymousElectorDocumentSearchService: AnonymousElectorDocumentSearchService
 
     @Test
-    fun `should return empty Anonymous Elector Document summaries`() {
+    fun `should return empty Anonymous Elector Document summaries given null searchBy`() {
         // Given
         val eroId = aValidRandomEroId()
         val gssCodes = listOf(aGssCode())
-        val dto = buildAnonymousSearchCriteriaDto(eroId = eroId)
+        val dto = buildAnonymousSearchCriteriaDto(eroId = eroId, searchBy = null)
 
         given(eroService.lookupGssCodesForEro(any())).willReturn(gssCodes)
         given(anonymousElectorDocumentSummaryRepository.findAllByGssCodeInAndSourceType(any(), any(), any())).willReturn(Page.empty())
@@ -59,17 +61,18 @@ internal class AnonymousElectorDocumentSearchServiceTest {
         verify(eroService).lookupGssCodesForEro(eroId)
         verify(anonymousElectorDocumentSummaryRepository).findAllByGssCodeInAndSourceType(gssCodes, ANONYMOUS_ELECTOR_DOCUMENT, buildPageRequest())
         verifyNoInteractions(anonymousSearchSummaryMapper)
+        verifyNoMoreInteractions(anonymousElectorDocumentSummaryRepository)
     }
 
     @Test
-    fun `should return Anonymous Elector Document summaries`() {
+    fun `should return Anonymous Elector Document summaries given null searchBy`() {
         // Given
         val eroId = aValidRandomEroId()
         val gssCodes = listOf(aGssCode())
         val aedSummary = buildAnonymousElectorDocumentSummaryEntity()
         val expectedSummaryDto = buildAnonymousSearchSummaryDto()
         val pageRequest = buildPageRequest(page = 2)
-        val dto = buildAnonymousSearchCriteriaDto(eroId = eroId, page = 2)
+        val dto = buildAnonymousSearchCriteriaDto(eroId = eroId, page = 2, searchBy = null)
 
         given(eroService.lookupGssCodesForEro(any())).willReturn(gssCodes)
         given(anonymousElectorDocumentSummaryRepository.findAllByGssCodeInAndSourceType(any(), any(), any()))
@@ -87,5 +90,67 @@ internal class AnonymousElectorDocumentSearchServiceTest {
         verify(eroService).lookupGssCodesForEro(eroId)
         verify(anonymousElectorDocumentSummaryRepository).findAllByGssCodeInAndSourceType(gssCodes, ANONYMOUS_ELECTOR_DOCUMENT, pageRequest)
         verify(anonymousSearchSummaryMapper).toAnonymousSearchSummaryDto(aedSummary)
+        verifyNoMoreInteractions(anonymousElectorDocumentSummaryRepository)
+    }
+
+    @Test
+    fun `should return empty Anonymous Elector Document summaries given searchBy surname`() {
+        // Given
+        val eroId = aValidRandomEroId()
+        val gssCodes = listOf(aGssCode())
+        val searchValue = "J-Smith O'Rorke   Junior"
+        val sanitizedSurname = "J SMITH ORORKE JUNIOR"
+        val dto = buildAnonymousSearchCriteriaDto(eroId = eroId, searchBy = AedSearchBy.SURNAME, searchValue = searchValue)
+
+        given(eroService.lookupGssCodesForEro(any())).willReturn(gssCodes)
+        given(anonymousElectorDocumentSummaryRepository.findAllByGssCodeInAndSourceTypeAndSanitizedSurname(any(), any(), any(), any())).willReturn(Page.empty())
+
+        // When
+        val actualPagedRecords =
+            anonymousElectorDocumentSearchService.searchAnonymousElectorDocumentSummaries(dto)
+
+        // Then
+        assertThat(actualPagedRecords).isNotNull
+        assertThat(actualPagedRecords.results).isNotNull.isEmpty()
+        verify(eroService).lookupGssCodesForEro(eroId)
+        verify(anonymousElectorDocumentSummaryRepository).findAllByGssCodeInAndSourceTypeAndSanitizedSurname(gssCodes, ANONYMOUS_ELECTOR_DOCUMENT, sanitizedSurname, buildPageRequest())
+        verifyNoInteractions(anonymousSearchSummaryMapper)
+        verifyNoMoreInteractions(anonymousElectorDocumentSummaryRepository)
+    }
+
+    @Test
+    fun `should return Anonymous Elector Document summaries given searchBy surname`() {
+        // Given
+        val eroId = aValidRandomEroId()
+        val gssCodes = listOf(aGssCode())
+        val aedSummary = buildAnonymousElectorDocumentSummaryEntity()
+        val expectedSummaryDto = buildAnonymousSearchSummaryDto()
+        val pageRequest = buildPageRequest(page = 2)
+        val searchValue = "J-Smith O'Rorke   Junior"
+        val sanitizedSurname = "J SMITH ORORKE JUNIOR"
+        val dto = buildAnonymousSearchCriteriaDto(
+            eroId = eroId,
+            page = 2,
+            searchBy = AedSearchBy.SURNAME,
+            searchValue = searchValue
+        )
+
+        given(eroService.lookupGssCodesForEro(any())).willReturn(gssCodes)
+        given(anonymousElectorDocumentSummaryRepository.findAllByGssCodeInAndSourceTypeAndSanitizedSurname(any(), any(), any(), any()))
+            .willReturn(PageImpl(listOf(aedSummary), pageRequest, 1))
+        given(anonymousSearchSummaryMapper.toAnonymousSearchSummaryDto(any())).willReturn(expectedSummaryDto)
+
+        val expected = AnonymousSearchSummaryResults(results = listOf(expectedSummaryDto))
+
+        // When
+        val actualPagedRecords =
+            anonymousElectorDocumentSearchService.searchAnonymousElectorDocumentSummaries(dto)
+
+        // Then
+        assertThat(actualPagedRecords).usingRecursiveComparison().isEqualTo(expected)
+        verify(eroService).lookupGssCodesForEro(eroId)
+        verify(anonymousElectorDocumentSummaryRepository).findAllByGssCodeInAndSourceTypeAndSanitizedSurname(gssCodes, ANONYMOUS_ELECTOR_DOCUMENT, sanitizedSurname, pageRequest)
+        verify(anonymousSearchSummaryMapper).toAnonymousSearchSummaryDto(aedSummary)
+        verifyNoMoreInteractions(anonymousElectorDocumentSummaryRepository)
     }
 }
