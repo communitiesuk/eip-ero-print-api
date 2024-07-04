902467209774ac089a6d6c485bfc1a9048b53bc9
Kirsty <kirsty.land@softwire.com>
2024-07-04 14:14:28 +0100

    EROPSPT-335: Turn off storing temporary certificates in S3

--- src/main/kotlin/uk/gov/dluhc/printapi/rest/TemporaryCertificateController.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/rest/TemporaryCertificateController.kt
@@ -2,6 +2,7 @@ package uk.gov.dluhc.printapi.rest
 
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.InputStreamResource
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpHeaders.CONTENT_DISPOSITION
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.dluhc.printapi.database.entity.SourceType
+import uk.gov.dluhc.printapi.dto.GenerateTemporaryCertificateDto
 import uk.gov.dluhc.printapi.dto.PdfFile
 import uk.gov.dluhc.printapi.exception.ResponseFileTooLargeException
 import uk.gov.dluhc.printapi.mapper.GenerateTemporaryCertificateMapper
@@ -45,6 +47,7 @@ class TemporaryCertificateController(
     private val statisticsUpdateService: StatisticsUpdateService,
     private val s3Service: S3PhotoService,
     private val generateTemporaryCertificateMapper: GenerateTemporaryCertificateMapper,
+    @Value("\${api.print-api.generate-temporary-certificate.upload-large-file-to-s3}") private val uploadLargeFileToS3: Boolean,
 ) {
 
     companion object {
@@ -96,17 +99,7 @@ class TemporaryCertificateController(
         )
         return temporaryCertificateService.generateTemporaryCertificate(eroId, dto).also {
             if (it.contents.size > MAX_RESPONSE_FILE_SIZE && generateTemporaryCertificateRequest.allowLargeResponse != true) {
-                val s3Path = "temporary_certificates/${dto.gssCode}/${dto.applicationReference}/${it.filename}"
-                logger.warn {
-                    "Response file for eroId = $eroId and sourceReference = ${dto.sourceReference} too large " +
-                        "to return Temporary VAC - putting to S3 path $s3Path instead"
-                }
-                s3Service.putObjectToTargetBucketFromByteArray(s3Path, it.contents)
-                throw ResponseFileTooLargeException(
-                    eroId,
-                    "Temporary VAC",
-                    dto.sourceReference
-                )
+                handleResponseFileTooLarge(it, eroId, dto)
             }
         }.also {
             statisticsUpdateService.triggerVoterCardStatisticsUpdate(dto.sourceReference)
@@ -138,5 +131,28 @@ class TemporaryCertificateController(
         headers.contentType = APPLICATION_PDF
         headers.add(CONTENT_DISPOSITION, "inline; filename=${pdfFile.filename}")
         return headers
+    }
+
+    private fun handleResponseFileTooLarge(pdfFile: PdfFile, eroId: String, dto: GenerateTemporaryCertificateDto) {
+        logger.warn {
+            "Response file for eroId = $eroId and sourceReference = ${dto.sourceReference} too large to return Temporary VAC"
+        }
+        if (uploadLargeFileToS3) {
+            val s3Path = "temporary_certificates/${dto.gssCode}/${dto.applicationReference}/${pdfFile.filename}"
+            logger.warn {
+                "Uploaded Temporary Certificate for ${dto.sourceReference} to S3 path $s3Path"
+            }
+            s3Service.putObjectToTargetBucketFromByteArray(s3Path, pdfFile.contents)
+        } else {
+            logger.warn {
+                "Not uploading Temporary Certificate for ${dto.sourceReference} to S3 path"
+            }
+        }
+
+        throw ResponseFileTooLargeException(
+            eroId,
+            "Temporary VAC",
+            dto.sourceReference
+        )
     }
 }
