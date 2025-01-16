8cee297abc5778211b91154fd60883b72178757e
Alex Yip <116652576+aleyipsoftwire@users.noreply.github.com>
2025-01-16 11:57:57 +0000

    EROPSPT-403: Update owasp suppressions (#326)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import java.lang.ProcessBuilder.Redirect
 
 plugins {
-    id("org.springframework.boot") version "3.3.5"
+    id("org.springframework.boot") version "3.3.7"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.25"
     kotlin("kapt") version "1.9.25"
