443196e40dfd43f3e742447515740ccc5a337ecf
Alex Yip <alex.yip@softwire.com>
2025-02-07 12:21:34 +0000

    EROPSPT-397: Return AED certificates with initial data removed

--- src/main/kotlin/uk/gov/dluhc/printapi/mapper/aed/ReIssueAnonymousElectorDocumentMapper.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/mapper/aed/ReIssueAnonymousElectorDocumentMapper.kt
@@ -6,6 +6,7 @@ import org.mapstruct.Mapping
 import org.mapstruct.MappingTarget
 import org.springframework.beans.factory.annotation.Autowired
 import uk.gov.dluhc.printapi.database.entity.AnonymousElectorDocument
+import uk.gov.dluhc.printapi.database.entity.AnonymousElectorDocumentDelivery
 import uk.gov.dluhc.printapi.database.entity.AnonymousElectorDocumentStatus
 import uk.gov.dluhc.printapi.dto.aed.ReIssueAnonymousElectorDocumentDto
 import uk.gov.dluhc.printapi.mapper.DeliveryAddressTypeMapper
@@ -80,7 +81,13 @@ abstract class ReIssueAnonymousElectorDocumentMapper {
         @MappingTarget aed: AnonymousElectorDocument,
         dto: ReIssueAnonymousElectorDocumentDto,
     ) {
-        aed.delivery?.deliveryAddressType = deliveryAddressTypeMapper.mapDtoToEntity(dto.deliveryAddressType)
+        val deliveryAddressTypeEntity = deliveryAddressTypeMapper.mapDtoToEntity(dto.deliveryAddressType)
+
+        if (aed.delivery != null) {
+            aed.delivery?.deliveryAddressType = deliveryAddressTypeEntity
+        } else {
+            aed.delivery = AnonymousElectorDocumentDelivery(deliveryAddressType = deliveryAddressTypeEntity)
+        }
     }
 
     @AfterMapping
