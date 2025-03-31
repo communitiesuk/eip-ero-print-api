ad2619e8278c445cd40bf9c562e3682aae15ad3a
Alex Yip <alex.yip@softwire.com>
2025-03-31 17:24:17 +0100

    EROPSPT-419: Upgrade SB to 3.4, dependency checker to 12, and remove some owasp suppressions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 import java.lang.ProcessBuilder.Redirect
 
 plugins {
-    id("org.springframework.boot") version "3.3.7"
+    id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version "1.9.25"
     kotlin("kapt") version "1.9.25"
@@ -16,7 +16,7 @@ plugins {
     id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
     id("org.jlleitschuh.gradle.ktlint-idea") version "11.0.0"
     id("org.openapi.generator") version "7.9.0"
-    id("org.owasp.dependencycheck") version "10.0.4" // version 11+ causes build failures, see https://github.com/spring-projects/spring-boot/issues/42952#issuecomment-2506304372
+    id("org.owasp.dependencycheck") version "12.1.0"
     id("org.jsonschema2dataclass") version "6.0.0"
 }
 
@@ -268,6 +268,7 @@ tasks.withType<KtLintCheckTask> {
 }
 
 tasks.withType<BootBuildImage> {
+    builder.set("paketobuildpacks/builder-jammy-base")
     environment.set(mapOf("BP_HEALTH_CHECKER_ENABLED" to "true"))
     buildpacks.set(
         listOf(
