41a0a51fc0a3eb1f49c5b0b0eecf73024e550265
ConWhiSW <connor.whitworth@softwire.com>
2026-07-27 09:48:00 +0100

    EROPSPT-729: Disable jobs

--- src/main/kotlin/uk/gov/dluhc/printapi/jobs/BatchPrintRequestsJob.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/jobs/BatchPrintRequestsJob.kt
@@ -1,9 +1,12 @@
 package uk.gov.dluhc.printapi.jobs
 
+import mu.KotlinLogging
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import uk.gov.dluhc.printapi.service.PrintRequestsService
+
+private val logger = KotlinLogging.logger {}
 
 @Component
 class BatchPrintRequestsJob(
@@ -13,6 +16,10 @@ class BatchPrintRequestsJob(
     @Scheduled(cron = "\${jobs.batch-print-requests.cron}")
     @SchedulerLock(name = "\${jobs.batch-print-requests.name}")
     fun run() {
-        printRequestsService.processPrintRequests()
+        // Crude way to disable the job, temp for DRT
+        if (1 == 0) {
+            printRequestsService.processPrintRequests()
+        }
+        logger.info("Batch print requests job run (disabled in DRT)")
     }
 }
--- src/main/kotlin/uk/gov/dluhc/printapi/jobs/ProcessPrintResponsesBatchJob.kt
+++ src/main/kotlin/uk/gov/dluhc/printapi/jobs/ProcessPrintResponsesBatchJob.kt
@@ -1,9 +1,12 @@
 package uk.gov.dluhc.printapi.jobs
 
+import mu.KotlinLogging
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import uk.gov.dluhc.printapi.service.PrintResponseFileReadinessService
+
+private val logger = KotlinLogging.logger {}
 
 @Component
 class ProcessPrintResponsesBatchJob(
@@ -13,6 +16,10 @@ class ProcessPrintResponsesBatchJob(
     @Scheduled(cron = "\${jobs.process-print-responses.cron}")
     @SchedulerLock(name = "\${jobs.process-print-responses.name}")
     fun pollAndProcessPrintResponses() {
-        printResponseFileReadinessService.markAndSubmitPrintResponseFileForProcessing()
+        // Crude way to disable the job, temp for DRT
+        if (1 == 0) {
+            printResponseFileReadinessService.markAndSubmitPrintResponseFileForProcessing()
+        }
+        logger.info { "Process print responses job run (disabled in DRT)" }
     }
 }
