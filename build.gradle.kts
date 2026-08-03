26e8c75d162cc8e30c9aec28a7c61a7f56ea38e9
MinLui <mina.lui@softwire.com>
2026-08-03 11:07:27 +0100

    EROPSPT-733: Bump messaging-support-lib tag

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
     // internal libs
     implementation("uk.gov.dluhc:logging-library:4.2.0")
     implementation("uk.gov.dluhc:bank-holidays-data-client-library:2.2.0")
-    implementation("uk.gov.dluhc:messaging-support-library:3.2.0")
+    implementation("uk.gov.dluhc:messaging-support-library:3.2.1")
     implementation("uk.gov.dluhc:email-client:1.4.0")
     implementation("uk.gov.dluhc:internal-auth-library:2.2.0")
 
